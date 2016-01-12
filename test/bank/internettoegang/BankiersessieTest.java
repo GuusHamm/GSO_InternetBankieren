6c9f389eb4316bbc9f239d9c233f70204f109cfe
binq1000 <t.daniels@student.fontys.nl>
2016-01-12 09:37:06 +0100

    BankierSessie test added

---
+++ test/bank/internettoegang/BankiersessieTest.java
@@ -0,0 +1,49 @@
+package bank.internettoegang;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nekkyou on 12-1-2016.
+ */
+public class BankiersessieTest
+{
+    
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+//    /**
+//     * @returns true als de laatste aanroep van getRekening of maakOver voor deze
+//     *          sessie minder dan GELDIGHEIDSDUUR geleden is
+//     *          en er geen communicatiestoornis in de tussentijd is opgetreden,
+//     *          anders false
+//     */
+    @Test
+    public void testIsGeldig() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testMaakOver() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testGetRekening() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testLogUit() throws Exception
+    {
+
+    }
+}
